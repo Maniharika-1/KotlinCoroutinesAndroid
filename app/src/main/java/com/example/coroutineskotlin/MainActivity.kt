715f39aaad9f4fb4b4b8623f054912a186e33ab1
Maniharika-1 <maniharika1998@gmail.com>
2022-05-24 17:51:15 +0530

    Switch the thread of coroutine using suspending function

---
+++ app/src/main/java/com/example/coroutineskotlin/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.coroutineskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnCount = findViewById<Button>(R.id.btnCount)
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+        val btnDownloadUserData = findViewById<Button>(R.id.btnDownloadUserData)
+
+        btnCount.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                tvCount.text = count++.toString()
+                Log.i("MyTagMain", "$count executing in ${Thread.currentThread().name}")
+            }
+        }
+        btnDownloadUserData.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch { downloadUserData() }
+        }
+    }
+
+    private suspend fun downloadUserData() {
+        val tvUserMessage = findViewById<TextView>(R.id.tvUserMessage)
+
+        for (i in 1..5000) {
+
+            withContext(Dispatchers.Main) {
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+
+        }
+    }
+}
