37d980efa11909adbca60f14d755c61920863c39
Maniharika-1 <maniharika1998@gmail.com>
2022-05-29 18:39:05 +0530

    Update MainActivity to call define MainActivityViewModel.

--- app/src/main/java/com/example/coroutineskotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutineskotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,37 +13,14 @@ import kotlinx.coroutines.withContext
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
+    private lateinit var mainActivityViewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCount = findViewById<Button>(R.id.btnCount)
-        val tvCount = findViewById<TextView>(R.id.tvCount)
-        val btnDownloadUserData = findViewById<Button>(R.id.btnDownloadUserData)
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        mainActivityViewModel.getUserData()
 
-        btnCount.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                tvCount.text = count++.toString()
-                Log.i("MyTagMain", "$count executing in ${Thread.currentThread().name}")
-            }
-        }
-        btnDownloadUserData.setOnClickListener {
-
-            CoroutineScope(Dispatchers.IO).launch { downloadUserData() }
-        }
-    }
-
-    private suspend fun downloadUserData() {
-        val tvUserMessage = findViewById<TextView>(R.id.tvUserMessage)
-
-        for (i in 1..5000) {
-
-            withContext(Dispatchers.Main) {
-                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
-            }
-
-        }
     }
 }
